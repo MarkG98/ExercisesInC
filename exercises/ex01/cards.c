5e3f681658b43b3adb97c19a0991466177928960
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-01-25 20:22:45 -0500

    Implemented update_count function

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,5 +1,32 @@
+/* Author: Mark Goldwater
+
+   Functionalized code from head first C which
+   is able to count cards based on user input.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Takes the current count and new card value to update count.
+   If card is between 2 and 7 up count by 1.
+   If card is 10, reduce count by 1.
+   The current count is then printed to the terminal.
+
+   count: current value of count
+   val: value of most recent card
+*/
+int update_count(int count, int val)
+{
+  if ((val > 2) && (val < 7)) {
+      count++;
+  } else if (val == 10) {
+      count--;
+  }
+
+  printf("Current count: %i\n", count);
+  return count;
+}
+
 int main()
 {
     char card_name[3];
@@ -26,12 +53,7 @@ int main()
                   continue;
               }
           }
-          if ((val > 2) && (val < 7)) {
-              count++;
-          } else if (val == 10) {
-              count--;
-          }
-          printf("Current count: %i\n", count);
+          count = update_count(count, val);
     } while (card_name[0] != 'X');
     return 0;
 }
