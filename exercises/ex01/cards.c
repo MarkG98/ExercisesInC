73f5ba6a3580da9d3b41d8321a5ccb6bb3222b27
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-01-26 19:22:32 -0500

    Split cards.c into three functions (including main) and get it to compile with the -ansi, -Wall, and -pedantic flags with no warnings

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 
 /* Takes the current count and new card value to update count.
+
    If card is between 2 and 7 up count by 1.
    If card is 10, reduce count by 1.
    The current count is then printed to the terminal.
@@ -17,24 +18,38 @@
 */
 int update_count(int count, int val)
 {
-  if ((val > 2) && (val < 7)) {
-      count++;
-  } else if (val == 10) {
-      count--;
-  }
-
-  printf("Current count: %i\n", count);
-  return count;
+    if ((val > 2) && (val < 7)) {
+        count++;
+    } else if (val == 10) {
+        count--;
+    }
+    printf("Current count: %i\n", count);
+    return count;
 }
+
+/* Prompts the user for input and puts the reply in the given buffer.
+
+   User input is truncated to the first two characters.
+
+   prompt: string prompt to display
+   card_name: buffer where result is stored
+*/
+void get_card_name(char *prompt, char *card_name)
+{
+    puts(prompt);
+    scanf("%2s", card_name);
+}
+
 
 int main()
 {
     char card_name[3];
+    char *prompt = "Enter the card name: ";
     int count = 0;
+    int val = 0;
     do {
-          puts("Enter the card_name: ");
-          scanf("%2s", card_name);
-          int val = 0;
+          get_card_name(prompt, card_name);
+
           switch(card_name[0]) {
           case 'K':
           case 'Q':
@@ -53,7 +68,9 @@ int main()
                   continue;
               }
           }
+
           count = update_count(count, val);
+
     } while (card_name[0] != 'X');
     return 0;
 }
