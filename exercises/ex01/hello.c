212a15a411267f4fd8d23cea4571eeb75d8c5ffb
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-01-27 20:04:42 -0500

    Finished optimization experiments

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,40 @@
+/*
+  Author: Mark Goldwater
+
+  Code for the optimization section of ex01 in ExercisesInC.
+
+  Questions:
+  1. Can you find the code that corresponds to this line?
+
+     subq	$16, %rsp
+     movl	$5, -4(%rbp)
+
+  2. What happens if you turn on optimizatio using the flag -02?
+
+     The number of assembly instructions is greatly reduced, and the
+     lines where x is assigned are no longer there.
+
+  3. Modify the printf statement to print x, then compile it with and
+     without optimization. What effect does it have when you print x?
+
+     Without optimization x is assigned and printed. With optimization, There
+     are less assemly instructions and __printf_chk is called rather
+     than printf. Also, it seems as the value of x is not stored, but just
+     printed.
+
+  4. And then print the value of y (but not x). Compile with and without
+     optimization. What happens to x and y?
+
+     It looks as if without optimization, x and y are assigned and then
+     y is printed. However, with optimization, it seems like the value
+     is just calculated and then printed.
+*/
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf("%i\n", y);
     return 0;
 }
