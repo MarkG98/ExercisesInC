533e1584df0bc43941676292f063d9e254cfcba1
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-02-03 18:26:10 -0500

    Finished stack experiments

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -3,6 +3,27 @@
 Copyright 2014 Allen Downey
 License: GNU GPLv3
 
+Questions:
+
+1. What this program is intended to do is print and return a pointer to an array
+   filled with 42 from the function foo(), print and create another array with bar()
+   where each element is its index, and then print the values of the array
+   returned by foo().
+
+2. The warning I try to compile this file is "function returns address of local
+   variable [-Wreturn-local-addr]", and it points to "return array in foo()".
+   This means that the array variable, a pointer to which is being returned, is
+   local to foo(). I am probably being warned of this because once this function
+   returns, this variable, and all other local variables, are destroyed.
+
+3. When I run the program, I get a Segmentation fault. This is because after foo()
+   return a pointer to array, and bar() is run, the program tries to print the
+   elements of array in main. However, after foo() returns, the array variable is
+   destroyed, so trying to do this in main is reading from an incorrect memory
+   location leading to a Segmentation fault.
+
+4. Now nothing prints and I still get a Segmentation fault.
+
 */
 
 #include <stdio.h>
@@ -14,7 +35,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    //printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +47,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    printf("%p\n", array);
+    //printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
