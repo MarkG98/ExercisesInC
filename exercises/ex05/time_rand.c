1b2a4ee6e10ed9c3ccadbbd25cd1c9aff4fb3f18
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-03-02 20:08:32 -0500

    Testing my_random_double against random_double

--- exercises/ex05/time_rand.c
+++ exercises/ex05/time_rand.c
@@ -2,6 +2,12 @@
 
 Copyright 2016 Allen B. Downey
 License: MIT License https://opensource.org/licenses/MIT
+
+Questions:
+
+4. "Dummy" is the fastest.
+6. "random_double" is faster than "my_random_double"
+
 */
 
 #include <stdio.h>
@@ -53,27 +59,27 @@ double time_func(int iters, float(*func)())
 }
 
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     double time;
     int iters = 100000000;
     int seed = 17;
 
-    time = time_func(iters, dummy);
-    printf("%f ms \t dummy\n", time);
+    // time = time_func(iters, dummy);
+    // printf("%f ms \t dummy\n", time);
+    //
+    // time = time_func(iters, dummy2);
+    // printf("%f ms \t dummy2\n", time);
+    //
+    // time = time_func(iters, random_float);
+    // printf("%f ms \t random_float\n", time);
+    //
+    // time = time_func(iters, my_random_float);
+    // printf("%f ms \t my_random_float\n", time);
 
-    time = time_func(iters, dummy2);
-    printf("%f ms \t dummy2\n", time);
+    time = time_func(iters, my_random_double);
+    printf("%f ms \t my_random_double\n", time);
 
-    time = time_func(iters, random_float);
-    printf("%f ms \t random_float\n", time);
-
-    time = time_func(iters, my_random_float);
-    printf("%f ms \t my_random_float\n", time);
-
-    time = time_func(iters, my_random_float2);
-    printf("%f ms \t my_random_float2\n", time);
-
-    time = time_func(iters, random_float);
-    printf("%f ms \t random_float\n", time);
+    time = time_func(iters, random_double);
+    printf("%f ms \t random_double\n", time);
 }
