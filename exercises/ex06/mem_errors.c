ab4174e3960f6f051f96001555b197fafba8b658
Mark Goldwater <mark.goldwater@students.olin.edu>
2020-03-09 22:47:34 -0400

    Adding corrected mem_errors.c

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -21,7 +21,7 @@ int read_element(int *array, int index) {
 
 int main()
 {
-    int never_allocated;
+    //int never_allocated;
     int *free_twice = malloc(sizeof (int));
     int *use_after_free = malloc(sizeof (int));
     int *never_free = malloc(sizeof (int));
@@ -33,12 +33,12 @@ int main()
     read_element(array1, 100);
 
     // but it does bounds-check dynamic arrays
-    read_element(array2, -1);
-    read_element(array2, 100);
+    read_element(array2, 6);
+    read_element(array2, 10);
 
     // and it catches use after free
-    free(use_after_free);
     *use_after_free = 17;
+    free(use_after_free);
 
     // never_free is definitely lost
     *never_free = 17;
@@ -47,10 +47,12 @@ int main()
     // free(&never_allocated);
 
     // but this one doesn't
-    free_anything(&never_allocated);
+    //free_anything(&never_allocated);
 
     free(free_twice);
-    free(free_twice);
+    free(never_free);
+    free(array2);
+    //free(free_twice);
 
     return 0;
 }
