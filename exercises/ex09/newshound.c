7fcc92cf606f3c94e880965d2437eec16b221071
Mark <goldwater243@gmail.com>
2020-04-11 22:09:44 -0400

    Completed newshound.c exercieses

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -3,6 +3,11 @@
 Downloaded from https://github.com/twcamper/head-first-c
 
 Modified by Allen Downey.
+
+1. Only articles from the first RSS feed is printed because once execle() is run,
+   this processes PID is passed to that function and then the program ends after
+   the script is run once and returns.
+
 */
 
 #include <stdio.h>
@@ -13,6 +18,16 @@ Modified by Allen Downey.
 #include <sys/types.h>
 #include <wait.h>
 
+// errno is an external global variable that contains
+// error information
+extern int errno;
+
+void wait_error(char *argv[])
+{
+    fprintf(stderr, "wait failed: %s\n", strerror(errno));
+    perror(argv[0]);
+    exit(1);
+}
 
 void error(char *msg)
 {
@@ -38,15 +53,39 @@ int main(int argc, char *argv[])
     int num_feeds = 5;
     char *search_phrase = argv[1];
     char var[255];
+    pid_t pid;
+    int status;
 
     for (int i=0; i<num_feeds; i++) {
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
 
-        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-        if (res == -1) {
-            error("Can't run script.");
+        pid = fork();
+
+        if (pid == -1)
+        {
+            error("Can't fork process.");
+        }
+
+        if (!pid)
+        {
+            int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+            if (res == -1) {
+                error("Can't run script.");
+            }
         }
     }
+
+    for (int i = 0; i < num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            wait_error(argv);
+        }
+
+        // check the exit status of the child
+        status = WEXITSTATUS(status);
+    }
+
     return 0;
 }
